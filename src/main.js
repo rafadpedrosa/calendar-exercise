25cc377981d9d095f3de47fff9c6c41a5b703209
Rafael Pedrosa <rpedrosa@rldatix.com>
2020-11-30 09:18:44 -0300

    Add emitter (works as eventBus)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import daysPlugin from './plugins/dayjs'
 import dayjs from "dayjs";
+import mitt from 'mitt'
 
-createApp(App)
-    .use(store)
+const emitter = mitt()
+
+const application = createApp(App)
+
+application.config.globalProperties.$emitter = emitter
+
+application.use(store)
     .use(router)
     .use(daysPlugin, dayjs)
     .mount('#app')
